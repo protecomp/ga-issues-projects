0a16909007123ad9362975eef8155e1af4d62717
Eero Vilpponen <eero.vilpponen@gmail.com>
2019-11-28 15:03:04 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
+const fetch = require('node-fetch');
 
 // most @actions toolkit packages have async methods
 async function run() {
   try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    const issue = core.getInput('issue');
+    const repository = core.getInput('repository');
+    const github_token = core.getInput('github_token');
 
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    let query = 'query($owner:String!, $name:String!){repository(owner: $owner, name: $name) {projects(first:1) {nodes {id name}}}}';
+    let variables = { owner: repository.split("/")[0], name: repository.split("/")[1] };
 
-    core.setOutput('time', new Date().toTimeString());
+    let response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify({query, variables}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `bearer ${github_token}`,
+      }
+    }).then(function(response) {
+      return response.json();
+    });
+    console.log(response);
+    const project = response['data']['repository']['projects']['nodes'][0];
+
+    console.log(`Adding issue ${issue} to ${project['name']}`);
+    console.log("");
+
+    query = 'mutation($issueId:ID!, $projectId:ID!) {updateIssue(input:{id:$issueId, projectIds:[$projectId]}) {issue {id}}';
+    variables = { issueId: issue, projectId: project['id'] };
+
+    response = await fetch('https://api.github.com/graphql', {
+      method: 'POST',
+      body: JSON.stringify({query, variables}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `bearer ${github_token}`,
+      }
+    }).then(function(response) {
+      return response.json();
+    });
+    console.log(response);
+    console.log(`Done!`)
   } 
   catch (error) {
     core.setFailed(error.message);
